72831096aa80a2d2d2ee99ea79e00878d4c523fc
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-12-07 10:34:49 +0100

    allow techeis

--- app/dashboard/departments/queries/get_dept.tsx
+++ app/dashboard/departments/queries/get_dept.tsx
@@ -1,40 +1,40 @@
 import { gql } from "@apollo/client";
 
 export const GET_DEPT = gql`
-subscription GetDeparment($company_id: uuid! , $limit: Int! , $offset: Int! ) {
-  departments(where: {company_id: {_eq: $company_id}}, limit: $limit, offset: $offset) {
-    abrev_code
-    agency_id
-    chief_department
-    company_id
-    status
-    created_at
-    id
-    name
-    parent_department_id
-    department {
+  subscription GetDeparment($company_id: uuid!, $limit: Int!, $offset: Int!) {
+    departments(where: {company_id: {_eq: $company_id}}, limit: $limit, offset: $offset, order_by: {created_at: desc}) {
+      abrev_code
+      agency_id
+      chief_department
+      company_id
+      status
+      created_at
       id
+      name
+      parent_department_id
+      department {
+        id
+        text_content {
+          id
+          content
+        }
+      }
       text_content {
         id
         content
       }
-    }
-    text_content {
-      id
-      content
-    }
-    services_aggregate {
-      aggregate {
-        count
+      services_aggregate {
+        aggregate {
+          count
+        }
+      }
+      employees_aggregate {
+        aggregate {
+          count
+        }
       }
     }
-    employees_aggregate {
-      aggregate {
-        count
-      }
-    }
-  }
-}`
+  }`;
 
 
 export const GET_DEPT_AGG = gql`
