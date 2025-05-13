a65072044ccf748129efbbd0306899f635614d93
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-05-13 17:21:56 +0100

    mutayion update

--- app/dashboard/reports/mutations/mutations.tsx
+++ app/dashboard/reports/mutations/mutations.tsx
@@ -1,24 +1,33 @@
 import { gql } from "@apollo/client";
 
 
-export const INSERT_REPORT = gql`mutation MyMutation(
+export const INSERT_REPORT = gql`
+mutation MyMutation(
   $categoryId: UUID!, 
   $fromDate: Date, 
   $toDate: Date, 
   $category1: CategoryTypeEnum!, 
   $reportType: ReportTypeEnum!
-) {
-  generateCsvReport(
-    input: {
-      category: $category1,
-      categoryId: $categoryId,
-      reportType: $reportType,
-      fromDate: $fromDate,
-      toDate: $toDate
+  ) 
+  {
+  generateCsvReport(input: {category: $category1, categoryId: $categoryId, reportType: $reportType, fromDate: $fromDate, toDate: $toDate}) {
+    type
+    attendanceData {
+      arrival
+      date
+      departure
+      employee
+      late
+      reason
+      status
     }
-  ) {
-    filename
-    reportLink
+    visitData {
+      checkIn
+      checkOut
+      date
+      reason
+      status
+      visitorName
+    }
   }
 }`;
-
