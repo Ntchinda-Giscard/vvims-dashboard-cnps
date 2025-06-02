d893f5e986ef76e59c0f75764e656321d87bd1e2
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-06-02 11:16:56 +0100

    update clockin

--- app/dashboard/attendance/mutation/clock_in.tsx
+++ app/dashboard/attendance/mutation/clock_in.tsx
@@ -1,11 +1,12 @@
 import { gql } from "@apollo/client";
 
 export const CLOCK_IN = gql`
-mutation ClockIn($employee_id: uuid!, $location: geometry = null) {
-  insert_attendance_one(object: {employee_id: $employee_id, location: $location, clock_in_date: now}) {
+mutation ClockIn($employee_id: uuid!, $location: geometry = null, $building_id: uuid="d94085cf-286a-4895-b346-14401c69736d") {
+  insert_attendance_one(object: {employee_id: $employee_id, location: $location, clock_in_date: now, building_id: $building_id}){
     id
   }
 }`;
+
 
 
 export const CLOCK_OUT = gql`
